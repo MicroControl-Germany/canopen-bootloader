1f721f8392d047d792dfcf29e5d097f6ff73a042
Uwe Koppe <koppe@microcontrol.net>
2023-06-26 10:19:29 +0200

    Documentation for version 4.08.00

---
+++ docs/menudata.js
@@ -0,0 +1,68 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Getting Started",url:"",children:[
+{text:"First steps",url:"cbl_getting_started.html"},
+{text:"Protocol Stack Overview",url:"cbl_api_overview.html"},
+{text:"Code Examples",url:"cbl_examples.html",children:[
+{text:"Initialisation",url:"cbl_example_start.html"},
+{text:"Flash Programming",url:"cbl_flash_programming.html"}]},
+{text:"Template Files",url:"cbl_api_templates.html",children:[
+{text:"Bootloader configuration",url:"cbl__conf_8h.html#CBL_CONF"},
+{text:"Bootloader start",url:"cbl_example_main.html"},
+{text:"Bootloader callbacks",url:"cbl_example_user.html"}]}]},
+{text:"Reference",url:"",children:[
+{text:"Bootloader Stack",url:"cbl_api_stack.html",children:[
+{text:"Object dictionary",url:"cbl__dict_8h.html#CBL_DICT"},
+{text:"EMCY service",url:"cbl__emcy_8h.html#CBL_EMCY"},
+{text:"LED support",url:"cbl__led_8h.html#CBL_LED"},
+{text:"LSS service",url:"cbl__lss_8h.html#CBL_LSS"},
+{text:"Management",url:"cbl__mgr_8h.html#CBL_MGR"},
+{text:"NMT service",url:"cbl__nmt_8h.html#CBL_NMT"},
+{text:"Object variables",url:"cbl__objs_8h.html#CBL_OBJS"},
+{text:"SDO service",url:"cbl__sdo_8h.html#CBL_SDO"},
+{text:"Timer support",url:"cbl__time_8h.html#CBL_TIME"},
+{text:"CANopen definitions",url:"canopen_8h.html#CANOPEN"},
+{text:"Stack definitions",url:"cbl__defs_8h.html#CBL_DEFS"}]},
+{text:"CANpie FD",url:"cbl_api_canpie.html",children:[
+{text:"Core",url:"cp__core_8h.html#CP_CORE_H"},
+{text:"FIFO",url:"cp__fifo_8h.html#CP_FIFO_H"},
+{text:"Message",url:"cp__msg_8h.html#CP_MSG_H"},
+{text:"Structures",url:"canpie_8h.html#CANPIE_H"}]},
+{text:"Hardware Abstraction Layer",url:"cbl_api_hal.html",children:[
+{text:"CPU",url:"mc__cpu_8h.html#MC_CPU"},
+{text:"Flash memory",url:"mc__flash_8h.html#MC_FLASH"},
+{text:"In-Application Programming",url:"mc__iap_8h.html#MC_IAP"},
+{text:"Non-volatile memory",url:"mc__nvm_8h.html#MC_NVM"},
+{text:"Timer",url:"mc__tmr_8h.html#MC_TMR"},
+{text:"Watchdog",url:"mc__wdt_8h.html#MC_WDT"},
+{text:"Compiler definitions",url:"mc__compiler_8h.html#MC_COMPILER_H"}]},
+{text:"Version history",url:"cbl_version.html"}]},
+{text:"Overviews",url:"",children:[
+{text:"Introduction to CAN",url:"intro_can.html"},
+{text:"References",url:"stack_references.html"},
+{text:"Support",url:"stack_support.html"},
+{text:"License",url:"page_8license.html"}]}]}
---
+++ docs/navtreedata.js
@@ -0,0 +1,87 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "CANopen (FD) Bootloader Protocol Stack", "index.html", [
+    [ "Getting Started", null, [
+      [ "First steps", "cbl_getting_started.html", null ],
+      [ "Protocol Stack Overview", "cbl_api_overview.html", null ],
+      [ "Code Examples", "cbl_examples.html", [
+        [ "Initialisation", "cbl_example_start.html", null ],
+        [ "Flash Programming", "cbl_flash_programming.html", null ]
+      ] ],
+      [ "Template Files", "cbl_api_templates.html", [
+        [ "Bootloader configuration", "cbl__conf_8h.html#CBL_CONF", null ],
+        [ "Bootloader start", "cbl_example_main.html", null ],
+        [ "Bootloader callbacks", "cbl_example_user.html", null ]
+      ] ]
+    ] ],
+    [ "Reference", null, [
+      [ "Bootloader Stack", "cbl_api_stack.html", [
+        [ "Object dictionary", "cbl__dict_8h.html#CBL_DICT", null ],
+        [ "EMCY service", "cbl__emcy_8h.html#CBL_EMCY", null ],
+        [ "LED support", "cbl__led_8h.html#CBL_LED", null ],
+        [ "LSS service", "cbl__lss_8h.html#CBL_LSS", null ],
+        [ "Management", "cbl__mgr_8h.html#CBL_MGR", null ],
+        [ "NMT service", "cbl__nmt_8h.html#CBL_NMT", null ],
+        [ "Object variables", "cbl__objs_8h.html#CBL_OBJS", null ],
+        [ "SDO service", "cbl__sdo_8h.html#CBL_SDO", null ],
+        [ "Timer support", "cbl__time_8h.html#CBL_TIME", null ],
+        [ "CANopen definitions", "canopen_8h.html#CANOPEN", null ],
+        [ "Stack definitions", "cbl__defs_8h.html#CBL_DEFS", null ]
+      ] ],
+      [ "CANpie FD", "cbl_api_canpie.html", [
+        [ "Core", "cp__core_8h.html#CP_CORE_H", null ],
+        [ "FIFO", "cp__fifo_8h.html#CP_FIFO_H", null ],
+        [ "Message", "cp__msg_8h.html#CP_MSG_H", null ],
+        [ "Structures", "canpie_8h.html#CANPIE_H", null ]
+      ] ],
+      [ "Hardware Abstraction Layer", "cbl_api_hal.html", [
+        [ "CPU", "mc__cpu_8h.html#MC_CPU", null ],
+        [ "Flash memory", "mc__flash_8h.html#MC_FLASH", null ],
+        [ "In-Application Programming", "mc__iap_8h.html#MC_IAP", null ],
+        [ "Non-volatile memory", "mc__nvm_8h.html#MC_NVM", null ],
+        [ "Timer", "mc__tmr_8h.html#MC_TMR", null ],
+        [ "Watchdog", "mc__wdt_8h.html#MC_WDT", null ],
+        [ "Compiler definitions", "mc__compiler_8h.html#MC_COMPILER_H", null ]
+      ] ],
+      [ "Version history", "cbl_version.html", null ]
+    ] ],
+    [ "Overviews", null, [
+      [ "Introduction to CAN", "intro_can.html", null ],
+      [ "References", "stack_references.html", null ],
+      [ "Support", "stack_support.html", null ],
+      [ "License", "page_8license.html", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"canopen_8h.html#CANOPEN"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
